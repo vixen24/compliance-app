fea5840e21e668b89f8596035be974cbcaf26a83
William Idakwo <jason2023.json@gmail.com>
2026-04-13 04:03:25 +0100

    we

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   require_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
 
+  layout "public"
   def new
   end
 
