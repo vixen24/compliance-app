4c903c1e4c7b7beacdff6812bfa1c900155addbf
William Idakwo <jason2023.json@gmail.com>
2026-05-12 17:29:59 +0100

    fix: error tenanting logic

--- db/migrate/20260425121334_add_subdomain_to_accounts.rb
+++ db/migrate/20260425121334_add_subdomain_to_accounts.rb
@@ -1,6 +1,6 @@
 class AddSubdomainToAccounts < ActiveRecord::Migration[8.2]
   def change
-    add_column :accounts, :subdomain, :string
+    add_column :accounts, :subdomain, :string, null: false
     add_index :accounts, :subdomain, unique: true
   end
 end
--- db/migrate/20260425121904_enforce_account_subdomain_not_null.rb
+++
@@ -1,5 +0,0 @@
-class EnforceAccountSubdomainNotNull < ActiveRecord::Migration[8.2]
-  def change
-    change_column_null :accounts, :subdomain, false
-  end
-end
